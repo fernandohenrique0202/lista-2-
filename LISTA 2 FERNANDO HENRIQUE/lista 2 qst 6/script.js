2b4a2d27cf012d9aeade9147f440607bb62c19ba
fernandohenrique0202 <fernando.costa105@etec.sp.gov.br>
2024-11-07 21:28:17 -0300

    Add files via upload

---
+++ LISTA 2 FERNANDO HENRIQUE/lista 2 qst 6/script.js
@@ -0,0 +1,12 @@
+var n1 = parseFloat(prompt("digite o primeiro numero"))
+var n2 = parseFloat(prompt("escreva o segundo numero"))
+var n3 = parseFloat(prompt("escreva o terceiro numero"))
+if (n1 > n2 && n3) {
+    alert("o maior numero é o "+ n1)
+}
+else if (n2 > n1 && n3) {
+    alert("o maior numero é o "+ n2)
+}
+else if (n3 > n1 && n2) {
+    alert("o maior numero é o "+ n3)
+}
