064b254d21c2b88b9d94e403aaf75a938fab93a4
fernandohenrique0202 <fernando.costa105@etec.sp.gov.br>
2024-11-07 22:07:04 -0300

    Add files via upload

---
+++ LISTA 2 FERNANDO HENRIQUE/lista 2 qst 10/script.js
@@ -0,0 +1,10 @@
+var turno = prompt("escreva a letra que refere a seu curso (m = matutino/v = vespertino/n = noturno")
+if (turno = "m"){
+    alert("Bom dia!")
+}
+else if (turno = "v"){
+    alert("Boa tarde!")
+}
+else if (turno = "n"){
+    alert("Boa noite!")
+}
