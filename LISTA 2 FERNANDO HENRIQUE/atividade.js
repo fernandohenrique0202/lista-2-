c522d2d021be216bae938e5ffafc98949835b529
fernandohenrique0202 <fernando.costa105@etec.sp.gov.br>
2024-11-17 22:48:17 -0300

    Add files via upload

---
+++ LISTA 2 FERNANDO HENRIQUE/atividade.js
@@ -0,0 +1,16 @@
+var numero = parseFloat(prompt("escreva um numero de 1 a 7"));
+if (numero == 1) {
+    alert("Domingo")
+} else if (numero == "2") {
+    alert("Segunda")
+} else if (numero == "3") {
+    alert("Terça")
+} else if (numero == "4") {
+    alert("quarta")
+} else if (numero == "5") {
+    alert("Quinta")
+} else if (numero == "6") {
+    alert("Sexta")
+} else if (numero == "7") {
+    alert("Sabado")
+}
