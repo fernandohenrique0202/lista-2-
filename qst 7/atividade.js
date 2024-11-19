05a3b98ed2169f06cad460273dee061f47ac9773
fernandohenrique0202 <fernando.costa105@etec.sp.gov.br>
2024-11-19 20:00:49 -0300

    Add files via upload

---
+++ qst 7/atividade.js
@@ -0,0 +1,15 @@
+var n1 = parseFloat(prompt("Escreva o primeiro numero"))
+var n2 = parseFloat(prompt("Escreva o segundo numero"))
+var n3 = parseFloat(prompt("Escreva o terceiro numero"))
+if (n1 >> n2 && n3) {
+    alert("o maior numero é o ", n1)
+} else if (n2 >> n3 && n1) {
+    alert("o maior numero é o ", n2)
+} else if (n3 >> n2 && n3) {
+    alert("o maior numero é o ", n3)
+} if (n1 << n2 && n3) {
+    alert("o menor numero é o ", n1)
+} else if (n2 << n3 && n1) {
+    alert("o menor numero é o ", n2)
+} else if (n3 << n2 && n3) {
+    alert("o menor numero é o ", n3)}
---
+++ qst 9/atividade.js
@@ -0,0 +1,19 @@
+var n1 = parseFloat(prompt("escreva o numero 1"))
+var n1 = parseFloat(prompt("escreva o numero 2"))
+var n1 = parseFloat(prompt("escreva o numero 3"))
+if (n1 > n2 > n3) {
+    alert("o maior é o primeiro. o medio o segundo e o menor o terceiro")
+} else if (n1 > n3 > n2) {
+    alert("o primeiro numero é maior o segundo numero é o menor e o terceiro numero é o medio")
+}
+else if (n2 > n1 > n3) {
+    alert("o primeiro numero é o medio segundo numero é o maior e o terceiro numero é o menor")
+}
+else if (n2 > n3 > n1) {
+    alert("o primeiro numero é o menor segundo numero é o maior e o terceiro numero é o medio")
+} else if (n3 > n1 > n2) {
+    alert("o primeiro numero é o medio segundo numero é o menor e o terceiro numero é o maior")
+}
+else if (n3>n2>n1){
+    alert("o primeiro é o menor, o segundo é o medio e o terceiro é o maior ")
+}
